3670367f8ada9f42a02a0b8a6addcc4b12c37e77
stephenCoderCamps <stephen.walther@codercamps.com>
2016-04-01 10:06:51 -0700

    added angularjs test helpers

--- src/WaxOnWaxOff/wwwroot/testScripts/stubs.js
+++ src/WaxOnWaxOff/wwwroot/testScripts/stubs.js
@@ -12,3 +12,33 @@ var console = {
     }
 }
 window.console = console;
+
+
+var AngularJSTestHelpers = {
+
+    
+
+    // checks specific element for binding (using mustache or ng-bind)
+    hasBinding: function (binding, element) {
+        if (!element) {
+            return false;
+        }
+        var matcher = new RegExp('({|\\s|^|\\|)' + binding + '(}|\\s|$|\\|)');
+        if (element.getAttribute('ng-bind') == binding) {
+            return true;
+        }
+        return matcher.test(element.innerHTML);
+    },
+
+    // checks whether document contains binding anywhere
+    hasBindingAnywhere: function (binding) {
+        var matcher = new RegExp('({|\\s|^|\\|)' + binding + '(}|\\s|$|\\|)');
+
+        // check for ng-bind
+        if (document.querySelector('[ng-bind="' + binding + '"')) {
+            return true;
+        }
+        // check for mustache
+        return matcher.test(document.documentElement.innerHTML);
+    }
+}
